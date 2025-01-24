fd4b216ec165e86071d2df217511d64044512ea9
Siddhant0413 <78699772+Siddhant0413@users.noreply.github.com>
2025-01-23 21:12:59 -0500

    Update RunProject1.java

---
+++ RunProject1.java
@@ -0,0 +1,9 @@
+/**
+ This is the driving class of the program that is directly linked to the run method
+ in the CollectionManager class. This will run the program and boot up the CollectionManager for inputs.
+ */
+public class RunProject1 {
+    public static void main(String[] args) {
+        new CollectionManager().run();
+    }
+}
