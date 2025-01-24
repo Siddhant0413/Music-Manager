b0606524c287e3365aa6f258e6aa392b876d6df7
Siddhant0413 <78699772+Siddhant0413@users.noreply.github.com>
2025-01-23 21:12:05 -0500

    Update Genre.java

---
+++ Genre.java
@@ -0,0 +1,7 @@
+/**
+ Enum class used to give an album a genre.
+ */
+//Creates the list of constants for the different types of Genres
+public enum Genre {
+    POP, COUNTRY, CLASSICAL, JAZZ, UNKNOWN
+}
